fad866a14daf63b2a90ef9f9ee46f46075aa9685
Dominik Karbowski <karbowski.dominik@gmail.com>
2019-02-18 17:30:21 +0100

    commit

---
+++ bo19e34arduino.ino
@@ -0,0 +1,32 @@
+// Visual Micro is in vMicro>General>Tutorial Mode
+// 
+/*
+    Name:       bo19e34arduino.ino
+    Created:	18.02.2019 17:19:18
+    Author:     DESKTOP-2KB0ATI\karbo
+*/
+
+// Define User Types below here or use a .h file
+//
+
+
+// Define Function Prototypes that use User Types below here or use a .h file
+//
+
+
+// Define Functions below here or use other .ino or cpp files
+//
+
+// The setup() function runs once each time the micro-controller starts
+void setup()
+{
+
+
+}
+
+// Add the main program code into the continuous loop() function
+void loop()
+{
+
+
+}
