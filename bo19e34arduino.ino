2db7892c0b80b1f8d0cd076a9589221b12c3e339
Ole Mikal <olemikole@hotmail.no>
2019-02-18 17:50:26 +0100

    Pasted from old repository

--- bo19e34arduino.ino
+++ bo19e34arduino.ino
@@ -1,32 +1,98 @@
-// Visual Micro is in vMicro>General>Tutorial Mode
-// 
+#include <Wire.h>
+#include <RTClib.h>	
+#include <SPI.h>
+#include <SD.h>
+
+#define fileName  "fil.txt"
+
+RTC_DS1307 rtc;
+File file;
+
+
 /*
-    Name:       bo19e34arduino.ino
-    Created:	18.02.2019 17:19:18
-    Author:     DESKTOP-2KB0ATI\karbo
+	SD
+		Clk	->	pin 13
+		DO ->	pin 12
+		DI ->	pin 11
+		CS ->	pin 10
+
+	RTC
+		SCL ->	A5
+		SDA ->	A4
+
 */
+void setup() {
 
-// Define User Types below here or use a .h file
-//
+	Serial.begin(9600);
+	SD.begin(10);
+	if (!rtc.begin())
+		Serial.println("Could not find RTC");
+	//rtc.adjust(DateTime(2019, 2, 8, 18, 30, 0));
+}
 
+void loop() {
 
-// Define Function Prototypes that use User Types below here or use a .h file
-//
+	if (Serial.available())
+	{
+		String inputString = Serial.readStringUntil('#');
 
+		char command = inputString.charAt(0);
+		if (command == 'w') writeToCard();
+		else if (command == 'r') readFromCard();
+		else if (command == 'e') SD.remove(fileName);
+		else if (command == 'c') adjustClock(inputString);
+	}
+}
 
-// Define Functions below here or use other .ino or cpp files
-//
-
-// The setup() function runs once each time the micro-controller starts
-void setup()
+void readFromCard()
 {
+	file = SD.open(fileName);
+	while (file.available())
+	{
+		Serial.write(file.read());
+	}
+	file.close();
+}
+
+void writeToCard()
+{
+	Serial.print("Saving: ");
+	file = SD.open(fileName, FILE_WRITE);
+
+	Serial.println(getTheDate());
+	file.println(getTheDate());
+	file.close();
 
 
 }
 
-// Add the main program code into the continuous loop() function
-void loop()
+void adjustClock(String inputString)
 {
+	//unsigned long inputString.substring(1,)
+	int year = inputString.substring(1, 5).toInt();
+	int month = inputString.substring(5, 7).toInt();
+	int date = inputString.substring(7, 9).toInt();
+	int hour = inputString.substring(9, 11).toInt();
+	int minute = inputString.substring(11, 13).toInt();
+	int second = inputString.substring(13, 15).toInt();
+	rtc.adjust(DateTime(year, month, date, hour, minute, second));
+}
 
-
+String getTheDate()
+{
+	String string = "";
+	char buffer[2];
+	DateTime now = rtc.now();
+	string += now.day();
+	string += '/';
+	string += now.month();
+	string += '/';
+	string += now.year();
+	string += '/';
+	string += now.hour();
+	string += '/';
+	string += now.minute();
+	string += '/';
+	string += now.second();
+	return string;
 }
